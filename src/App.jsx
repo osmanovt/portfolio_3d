1409d2357ff38940318955d0a5517ea3db2206a8
TO <osmanov.tural2013@yandex.ru>
2025-04-14 17:03:34 +0300

    add: TestimonialsSection

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import LogoSection from './sections/LogoSection.jsx';
 import FeatureCards from './sections/FeatureCards.jsx';
 import ExperienceSection from './sections/ExperienceSection.jsx';
 import TechStackSection from './sections/TechStackSection.jsx';
+import TestimonialSection from './sections/TestimonialSection.jsx';
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <FeatureCards />
       <ExperienceSection />
       <TechStackSection />
+      <TestimonialSection />
     </>
   );
 };
---
+++ src/sections/TestimonialsSection.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { testimonials } from '../constants/index.js';
+import GlowCard from '../components/GlowCard.jsx';
+import TitleHeader from '../components/TitleHeader.jsx';
+
+function TestimonialSection () {
+  return (
+    <section id="testimonials" className="flex-center section-padding">
+      <div className="w-full h-full md:px-10 px-5">
+        <TitleHeader
+          title="What People Say About Me?"
+          sub="👥 Client Feedback Highlights"
+        />
+        <div className="lg:colums-3 md:columns-2 columns-1 mt-16">
+          {testimonials.map(({name, imgPath, mentions, review}) => (
+            <GlowCard card={{review}}>
+              <div  className="flex items-center gap-3">
+                <div>
+                  <img src={imgPath} alt={name}/>
+                </div>
+                <div className="">
+                  <p className="font-bold">{name}</p>
+                  <p className="text-white-50">{mentions}</p>
+                </div>
+              </div>
+            </GlowCard>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default TestimonialSection;
